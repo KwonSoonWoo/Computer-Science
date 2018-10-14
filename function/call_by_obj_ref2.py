f80f9d039c94c0d20f5cf4bbcf940b3898600d84
ksw <zenuine1@gmail.com>
2018-10-14 16:33:17 +0900

    함수-인자전달방식에 따른 분류 업데이트

---
+++ function/call_by_obj_ref2.py
@@ -0,0 +1,7 @@
+def func(li):
+    li[0] = 'I am your father!'
+
+if __name__ == "__main__":
+    li = [1, 2, 3, 4]
+    func(li)
+    print(li)
---
+++ function/call_by_obj_ref3.py
@@ -0,0 +1,8 @@
+def func(li):
+    li = ['I am your father', 2, 3, 4]
+
+
+if __name__ == "__main__":
+    li = [1, 2, 3, 4]
+    func(li)
+    print(li)
